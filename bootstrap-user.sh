008b025c6c6e1c984016358bf8d1fe53dcd462a3
Ioannis Latousakis <latusaki@users.noreply.github.com>
2019-09-03 22:42:58 +0100

    Update bootstrap-user.sh

--- bootstrap-user.sh
+++ bootstrap-user.sh
@@ -16,9 +16,10 @@ echo "eval \"\$(pyenv virtualenv-init -)\"" >> $HOME/.bashrc
 source $HOME/.bashrc
 
 echo "pyenv installation complete"
+
 echo "Setup dotfiles"
 cd
-mkdir .vim/colors/
+mkdir -p ~/.vim/colors/
 git clone https://github.com/latusaki/dotfiles.git
 cp dotfiles/.vimrc ~/.vimrc
 cp dotfiles/solarized.vim ~/.vim/colors/.
