a52983c266d9564e2b3c46956b3ecda588f3bf4d
Latousakis <ilatousakis@uk.imshealth.com>
2019-10-15 09:42:32 +0100

    Update bootstrap files

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,5 @@
+echo "Update aptget"
+
 sudo apt-get update
 sudo apt-get upgrade
 
@@ -8,17 +10,28 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 sudo apt-get update
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 sudo apt-get --assume-yes  install docker-ce docker-ce-cli containerd.io
 
 echo "Docker installation complete"
 echo ""
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo apt-get install -y maven
+
 echo "Install python dev requirements"
 
-sudo apt-get install -y build-essential
-sudo apt-get install -y libbz2-dev libsqlite3-dev llvm libncurses5-dev libncursesw5-dev tk-dev liblzma-dev
-sudo apt-get install -y python3-dev python3-venv
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev
+libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
+
+sudo apt-get install -y python3-dev python3-venv libsnappy-dev r-base
 
 echo "Python requirements installed"
 
 sudo apt-get install -y silversearcher-ag
+
